a152e5e5d268fabf3455fec1d0744c1765c059d6
EvstafevArtem <evstafev.as@phystech.edu>
2024-04-27 20:32:13 +0300

    did a DFS

--- test.cpp
+++ test.cpp
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <stack>
 
+#include "src/readCSV.h"
+
 struct TH {
     int v;
     float w;
@@ -33,14 +35,30 @@ public:
         return w[num];
     }
 
-    void changeColumn(int row, int num)
+    std::vector<bool> changeColumn(int row)
     {
+        std::vector<bool> change(n);
         for (int j = 0; j < n; ++j)
         {
             if (A[row * n + j] == 0)
             {
+                change[j] = true;
                 for (int i = 0; i < n; ++i)
-                    A[n * i + j] += num;
+                    A[n * i + j] += 10;
+            }
+        }
+        return change;
+    }
+
+    void undoChangeColumn(const std::vector<bool> toChange)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            auto ds = toChange.size();
+            if (toChange[j])
+            {
+                for (int i = 0; i < n; ++i)
+                    A[n * i + j] -= 10;
             }
         }
     }
@@ -61,6 +79,7 @@ std::pair<std::vector<int>, float> DepthFirstSearch(Graph &graph, const int star
     std::vector<int> enter_order;
     std::vector<short> visited(n);
     std::stack<int> s;
+    std::stack<std::vector<bool>> changes;
 
     visited[start_vertex] = true;
     s.push(start_vertex);
@@ -83,6 +102,8 @@ std::pair<std::vector<int>, float> DepthFirstSearch(Graph &graph, const int star
                 is_found = true;
                 visited[to] = true;
                 s.push(to);
+                changes.push(graph.changeColumn(to));
+
                 enter_order.emplace_back(to);
                 sum_weight += graph(to);
                 if (sum_weight > max_weight)
@@ -97,6 +118,7 @@ std::pair<std::vector<int>, float> DepthFirstSearch(Graph &graph, const int star
         if (!is_found)
         {
             s.pop();
+            graph.undoChangeColumn(changes.top());
             sum_weight -= graph(from);
         }
     }
@@ -104,46 +126,56 @@ std::pair<std::vector<int>, float> DepthFirstSearch(Graph &graph, const int star
     return {longets_path, max_weight};
 }
 
+const std::string FILE_PATH = __FILE__;
+const int amountOfSymbolsBeforeRootDirectory = 8;
+const std::string DIR_PATH = FILE_PATH.substr(0, FILE_PATH.size() - amountOfSymbolsBeforeRootDirectory);
+
 int main() {
 
-    std::vector<int> A(25);
-    for (int i = 0; i < 5; ++i)
-        A[i * 5 + i] = 0;
-    A[1 * 5 + 0] = A[0 * 5 + 1] = 1;
-    A[2 * 5 + 0] = A[0 * 5 + 2] = 1;
-    A[1 * 5 + 3] = A[3 * 5 + 1] = 1;
-    A[1 * 5 + 4] = A[4 * 5 + 1] = 1;
+//    std::vector<int> A(25);
+//    for (int i = 0; i < 5; ++i)
+//        A[i * 5 + i] = 0;
+//    A[1 * 5 + 0] = A[0 * 5 + 1] = 1;
+//    A[2 * 5 + 0] = A[0 * 5 + 2] = 1;
+//    A[1 * 5 + 3] = A[3 * 5 + 1] = 1;
+//    A[1 * 5 + 4] = A[4 * 5 + 1] = 1;
 
-    for (int i = 0; i < 5; ++i)
-    {
-        for (int j = 0; j < 5; ++j)
-            std::cout<<A[i * 5 + j]<<' ';
-        std::cout<<std::endl;
-    }
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        for (int j = 0; j < 5; ++j)
+//            std::cout<<A[i * 5 + j]<<' ';
+//        std::cout<<std::endl;
+//    }
+//
+//    for (int j = 0; j < 5; ++j)
+//    {
+//        if (A[3 * 5 + j] == 0)
+//        {
+//            for (int i = 0; i < 5; ++i)
+//                A[5 * i + j] += 1;
+//        }
+//    }
+//
+//
+//    std::cout<<std::endl;
+//
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        for (int j = 0; j < 5; ++j)
+//            std::cout<<A[i * 5 + j]<<' ';
+//        std::cout<<std::endl;
+//    }
 
-    for (int j = 0; j < 5; ++j)
-    {
-        if (A[3 * 5 + j] == 0)
-        {
-            for (int i = 0; i < 5; ++i)
-                A[5 * i + j] += 1;
-        }
-    }
+    std::vector<int> A = readCSV(DIR_PATH + "resources/matrix_only.csv");
+    std::vector<float> w = {6.7871, 0.4965, 0.5813,6.3638,7.7534,6.7388,2.1945,5.7203,2.5396,0.0394,12.3052,5.3397,
+                          0.7031, 4.514,1.4971, 5.853,1.8569,3.5543,1.9219,11.8509,2.2925,5.1243,9.2748,2.9068,
+                          5.4177, 3.3817, 3.5976, 0.5599};
 
 
-    std::cout<<std::endl;
-
-    for (int i = 0; i < 5; ++i)
-    {
-        for (int j = 0; j < 5; ++j)
-            std::cout<<A[i * 5 + j]<<' ';
-        std::cout<<std::endl;
-    }
-
-//    Graph g(A, {0.1, 0.1, 0.1, 0.1, 0.1});
-//    auto res = DepthFirstSearch(g, 0);
-//    for (const auto el: res.first)
-//        std::cout << el;
-//    std::cout<<' '<<res.second;
+    Graph g(A, w);
+    auto res = DepthFirstSearch(g, 0);
+    for (const auto el: res.first)
+        std::cout << el << ' ';
+    std::cout<<' '<<res.second;
     return 0;
 }
